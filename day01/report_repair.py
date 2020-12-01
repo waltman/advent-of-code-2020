7ebdf24983792eff311844d4e0ab6c3cda386081
Walt Mankowski <waltman@pobox.com>
2020-12-01 06:37:06 -0500

    added code for part 2
    also cleaned up the output a little

--- day01/report_repair.py
+++ day01/report_repair.py
@@ -10,4 +10,10 @@ for pair in combinations(entries, 2):
     if sum(pair) == 2020:
         res = pair[0] * pair[1]
         break
-print(res)
+print('Part 1:', res)
+
+for pair in combinations(entries, 3):
+    if sum(pair) == 2020:
+        res = pair[0] * pair[1] * pair[2]
+        break
+print('Part 2:', res)
