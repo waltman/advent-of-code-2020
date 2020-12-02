2fb70d7bcf1bf8bd060665216a651d9146916708
Walt Mankowski <waltman@pobox.com>
2020-12-02 11:16:58 -0500

    Perl code for day 2

---
+++ day02/password_philosophy.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature qw(:5.32);
+use experimental qw(signatures);
+
+sub letter_count($letter, @password_letters) {
+    my $cnt = 0;
+    for my $c (@password_letters) {
+        $cnt++ if $c eq $letter;
+    }
+    return $cnt;
+}
+
+sub is_valid($letter, $p1, $p2, @password_letters) {
+    return ($password_letters[$p1-1] eq $letter) ^ $password_letters[$p2-1] eq $letter;
+}
+
+my $count1 = 0;
+my $count2 = 0;
+
+while (<>) {
+    chomp;
+    my ($times, $letter, $password) = split;
+    my ($start, $end) = split '-', $times;
+    $letter =~ s/://;
+    my @password_letters = split //, $password;
+    $count1++ if $start <= letter_count($letter, @password_letters) <= $end;
+    $count2++ if is_valid($letter, $start, $end, @password_letters);
+}
+
+say "Part 1: $count1";
+say "Part 2: $count2";
