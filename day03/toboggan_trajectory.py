17d20fd515055504ef1aa7318dc76cf463b1a58c
Walt Mankowski <waltman@pobox.com>
2020-12-03 07:05:58 -0500

    updated python code to solve part 2
    This mainly involved moving the tree-counting logic to num_trees()

--- day03/toboggan_trajectory.py
+++ day03/toboggan_trajectory.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 from sys import argv
 
+def num_trees(tree_map, num_rows, num_cols, dr, dc):
+    trees = 0
+    row,col = 0,0
+
+    while row < num_rows:
+        if tree_map[row][col] == '#':
+            trees += 1
+
+        row += dr
+        col = (col + dc) % num_cols
+
+    return trees
+
 tree_map = []
 filename = argv[1]
 with open(filename) as f:
@@ -10,15 +23,21 @@ num_rows = len(tree_map)
 num_cols = len(tree_map[0])
 
 # Part 1
-row, col = 0,0
 dr,dc = 1,3
-num_trees = 0
-while row < num_rows:
-    if tree_map[row][col] == '#':
-        num_trees += 1
+print('Part 1:', num_trees(tree_map, num_rows, num_cols, dr, dc))
 
-    row += dr
-    col = (col + dc) % num_cols
+# Part 2
+slopes = [
+    [1,1],
+    [3,1],
+    [5,1],
+    [7,1],
+    [1,2],
+]
 
-print('Part 1:', num_trees)
+tree_prod = 1
+for slope in slopes:
+    dc,dr = slope
+    tree_prod *= num_trees(tree_map, num_rows, num_cols, dr, dc)
 
+print('Part 2:', tree_prod)
