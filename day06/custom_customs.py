213ad6a60e87ddfbdd5711af7e2c3bbb14949d7a
Walt Mankowski <waltman@pobox.com>
2020-12-06 06:33:45 -0500

    updated to solve day 6 part 2

--- day06/custom_customs.py
+++ day06/custom_customs.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 from sys import argv
+from collections import defaultdict
 
-questions = set()
+questions = defaultdict(int)
 num_groups = 0
+num_all = 0
+group_size = 0
 filename = argv[1]
 with open(filename) as f:
     for line in f:
         line = line.rstrip()
         if line:
+            group_size += 1
             for q in line:
-                questions.add(q)
+                questions[q] += 1
         else:
             num_groups += len(questions)
+            # how many questions did everyone answer?
+            num_all += len([v for v in questions.values() if v == group_size])
+
             questions.clear()
+            group_size = 0
 
 print('Part 1:', num_groups)
+print('Part 2:', num_all)
