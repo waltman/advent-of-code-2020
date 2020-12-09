8d47e5efea6c9bbd0aefea17dda8f58bbefb99d4
Walt Mankowski <waltman@pobox.com>
2020-12-09 07:30:55 -0500

    python code for day 9 part 2

--- day09/encoding_error.py
+++ day09/encoding_error.py
@@ -9,6 +9,19 @@ with open(filename) as f:
 PREAMBLE = 25
 for i in range(PREAMBLE, len(data)):
     if data[i] not in [sum(comb) for comb in combinations(data[i-PREAMBLE:i], 2)]:
-        print('Part 1:', data[i])
+        invalid_number = data[i]
         break
+print('Part 1:', invalid_number)
 
+# solve part 2
+found = False
+for i in range(len(data)):
+    if found:
+        break
+    for j in range(i+1, len(data)+1):
+        if (tot := sum(data[i:j]) == invalid_number):
+            print('Part 2:', min(data[i:j]) + max(data[i:j]))
+            found = True
+            break
+        elif tot > invalid_number:
+            break
