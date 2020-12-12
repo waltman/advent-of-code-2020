59061c212ab297e3de064018d4477cb79bae7ab5
Walt Mankowski <waltman@pobox.com>
2020-12-12 09:18:11 -0500

    updated to solve day 12 part 2

--- day12/rain_risk.py
+++ day12/rain_risk.py
@@ -48,7 +48,46 @@ class Navigate:
         for _ in range(value // 90):
             self.direction = self.next_left[self.direction]
         
+class Waypoint(Navigate):
+    def __init__(self):
+        Navigate.__init__(self)
+        self.waypoint = [1,10]
+        
+    def move(self, action, value):
+        if action == 'N':
+            self.waypoint[0] += value
+        elif action == 'S':
+            self.waypoint[0] -= value
+        elif action == 'E':
+            self.waypoint[1] += value
+        elif action == 'W':
+            self.waypoint[1] -= value
+        elif action == 'F':
+            self.pos[0] += value * self.waypoint[0]
+            self.pos[1] += value * self.waypoint[1]
+        elif action == 'R':
+            self.turn_right(value)
+        elif action == 'L':
+            self.turn_left(value)
+
+    def turn_right(self, value):
+        if value == 90:
+            self.waypoint = [-self.waypoint[1], self.waypoint[0]]
+        elif value == 180:
+            self.waypoint = [-self.waypoint[0], -self.waypoint[1]]
+        elif value == 270:
+            self.waypoint = [self.waypoint[1], -self.waypoint[0]]
+        
+    def turn_left(self, value):
+        if value == 90:
+            self.waypoint = [self.waypoint[1], -self.waypoint[0]]
+        elif value == 180:
+            self.waypoint = [-self.waypoint[0], -self.waypoint[1]]
+        elif value == 270:
+            self.waypoint = [-self.waypoint[1], self.waypoint[0]]
+        
 nav = Navigate()
+wp = Waypoint()
 filename = argv[1]
 with open(filename) as f:
     for line in f:
@@ -56,5 +95,7 @@ with open(filename) as f:
         action = line[0]
         value = int(line[1:])
         nav.move(action, value)
+        wp.move(action, value)
 
 print('Part 1:', nav.dist())
+print('Part 2:', wp.dist())
