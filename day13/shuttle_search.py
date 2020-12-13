ba779cc89baeaec113c95caa585dcca7e4ae0909
Walt Mankowski <waltman@pobox.com>
2020-12-13 07:33:20 -0500

    added code to solve day 13 part 2

--- day13/shuttle_search.py
+++ day13/shuttle_search.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 from sys import argv
+from sympy.ntheory.modular import crt
 
 filename = argv[1]
+m = []
+v = []
 with open(filename) as f:
     start_time = int(f.readline().rstrip())
-    ids = [int(i) for i in f.readline().rstrip().split(',') if i != 'x']
+    ids_raw = f.readline().rstrip().split(',')
+    ids = [int(i) for i in ids_raw if i != 'x']
+    for i in range(len(ids_raw)):
+        if ids_raw[i] != 'x':
+            n = int(ids_raw[i])
+            m.append(n)
+            v.append(0 if i == 0 else n-i)
 
 i = 0
 found = False
@@ -16,3 +25,5 @@ while not found:
             found = True
             break
     i += 1
+
+print('Part 2:', crt(m, v)[0])
