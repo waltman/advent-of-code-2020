63b4103af19a0f2ae6dede027ab6068e72eaedf1
Walt Mankowski <waltman@pobox.com>
2020-12-14 08:46:50 -0500

    updated code to solve part 2

--- day14/docking_data.py
+++ day14/docking_data.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 from sys import argv
 from collections import defaultdict
+from itertools import product
+from copy import deepcopy
 import re
 
+def mask_addrs(mask, addr):
+    template = []
+    xpos = []
+    for i, m in enumerate(mask):
+        if m == '0':
+            template.append('1' if addr & 1<<(35-i) else '0')
+        elif m == '1':
+            template.append('1')
+        else:
+            template.append('X')
+            xpos.append(i)
+
+    for p in product('01', repeat=len(xpos)):
+        t = deepcopy(template)
+        for i in range(len(p)):
+            t[xpos[i]] = p[i]
+        yield int(''.join(t), 2)
+
 mem = defaultdict(int)
+mem2 = defaultdict(int)
 filename = argv[1]
 with open(filename) as f:
     for line in f:
@@ -17,6 +38,8 @@ with open(filename) as f:
             k = int(m.group(1))
             v = int(m.group(2))
             mem[k] = (v | one_mask) & zero_mask
+            for addr in mask_addrs(mask, k):
+                mem2[addr] = v
 
 print('Part 1:', sum(mem.values()))
-
+print('Part 2:', sum(mem2.values()))
