6f0545814edc8c240e4d3dff048a9bd03b93ccbe
Walt Mankowski <waltman@pobox.com>
2020-12-15 06:43:01 -0500

    Python code for Day 15 part 1

---
+++ day15/rambunctious_recitation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from sys import argv
+
+last_seen = {}
+for i,val in enumerate(argv[1].split(',')):
+    last_seen[int(val)] = (i+1,)
+    last = int(val)
+
+for t in range(len(last_seen) + 1, 2021):
+    prev = last_seen[last]
+    if len(prev) == 1:
+        next_val = 0
+    else:
+        next_val = prev[1] - prev[0]
+    if next_val in last_seen:
+        old = last_seen[next_val]
+        i = 1 if len(old) == 2 else 0
+        last_seen[next_val] = (old[i], t)
+    else:
+        last_seen[next_val] = (t,)
+    last = next_val
+
+print('Part 1:', last)
