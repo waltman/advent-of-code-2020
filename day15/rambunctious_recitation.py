f34ca975bf9c728c04c4c1ca22712ef8abe5d80c
Walt Mankowski <waltman@pobox.com>
2020-12-15 10:09:14 -0500

    added code to solve day 15 part 2

--- day15/rambunctious_recitation.py
+++ day15/rambunctious_recitation.py
@@ -12,6 +12,7 @@ for t in range(len(last_seen) + 1, 2021):
         next_val = 0
     else:
         next_val = prev[1] - prev[0]
+
     if next_val in last_seen:
         old = last_seen[next_val]
         i = 1 if len(old) == 2 else 0
@@ -21,3 +22,20 @@ for t in range(len(last_seen) + 1, 2021):
     last = next_val
 
 print('Part 1:', last)
+
+for t in range(2021, 30_000_001):
+    prev = last_seen[last]
+    if len(prev) == 1:
+        next_val = 0
+    else:
+        next_val = prev[1] - prev[0]
+
+    if next_val in last_seen:
+        old = last_seen[next_val]
+        i = 1 if len(old) == 2 else 0
+        last_seen[next_val] = (old[i], t)
+    else:
+        last_seen[next_val] = (t,)
+    last = next_val
+
+print('Part 2:', last)
