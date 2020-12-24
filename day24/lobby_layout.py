6ba0e50fe44840e9479597ad1e6e35e95cfddda5
Walt Mankowski <waltman@pobox.com>
2020-12-24 10:56:52 -0500

    updated code to solve day 24 part 2

--- day24/lobby_layout.py
+++ day24/lobby_layout.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from sys import argv
-from collections import defaultdict
 
 def get_coords(line):
     r,c = 0,0
@@ -30,15 +29,54 @@ def get_coords(line):
             i += 2
     return r,c
 
-tiles = defaultdict(int)
+def neighbors(r,c):
+    return [(r,c+2),
+            (r,c-2),
+            (r+1,c-1),
+            (r+1,c+1),
+            (r-1,c-1),
+            (r-1,c+1)]
+
+def rc_key(r,c):
+    return f'{r},{c}'
+
+def black_neighbors(tiles, r, c):
+    cnt = 0
+    for n in neighbors(r,c):
+        r1,c1 = n
+        cnt += tiles[rc_key(r1,c1)]
+    return cnt
+
+def run_day(tiles):
+    flip = []
+    for r in range(-300, 301):
+        for c in range(-300, 301):
+            k = rc_key(r,c)
+            bn = black_neighbors(tiles, r, c)
+            if tiles[k] == 1 and (bn == 0 or bn > 2):
+                flip.append(k)
+            elif tiles[k] == 0 and bn == 2:
+                flip.append(k)
+    for k in flip:
+        tiles[k] = (tiles[k] + 1) % 2
+
+tiles = {}
+for r in range(-500,501):
+    for c in range(-550,500):
+        tiles[rc_key(r,c)] = 0
 filename = argv[1]
 with open(filename) as f:
     for line in f:
         line = line.rstrip()
         r,c = get_coords(line)
-        k = f'{r},{c}'
+        k = rc_key(r,c)
         tiles[k] = (tiles[k] + 1) % 2
 
 print('Part 1:', sum(tiles.values()))
 
-        
+for day in range(1, 101):
+    run_day(tiles)
+    if day % 10 == 0:
+        print(f'Day {day}: {sum(tiles.values())}')
+
+print('Part 2:', sum(tiles.values()))
