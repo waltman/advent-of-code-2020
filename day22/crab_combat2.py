daeb8e2a1e1dedb2621f840ccd060bbd71426e16
Walt Mankowski <waltman@pobox.com>
2020-12-23 20:08:31 -0500

    Python code for Day 22 Part 2

--- day22/crab_combat2.py
+++ day22/crab_combat2.py
@@ -10,6 +10,37 @@ def score(q):
         val -= 1
     return tot
 
+def play(p1, p2):
+    seen = set()
+    while len(p1) and len(p2):
+        k = str(list(p1)) + str(list(p2))
+        if k in seen:
+            return p1, None
+        else:
+            seen.add(k)
+
+        c1 = p1.popleft()
+        c2 = p2.popleft()
+
+        # play subgame?
+        if c1 <= len(p1) and c2 <= len(p2):
+            new_p1, new_p2 = play(deque(list(p1)[:c1]), deque(list(p2)[:c2]))
+            if new_p1:
+                p1.append(c1)
+                p1.append(c2)
+            else:
+                p2.append(c2)
+                p2.append(c1)
+        else:
+            if c1 > c2:
+                p1.append(c1)
+                p1.append(c2)
+            else:
+                p2.append(c2)
+                p2.append(c1)
+
+    return p1, p2
+
 p1 = deque()
 p2 = deque()
 filename = argv[1]
@@ -27,19 +58,11 @@ with open(filename) as f:
                 p2.append(int(line))
 
 # play the game
-while len(p1) and len(p2):
-    c1 = p1.popleft()
-    c2 = p2.popleft()
-    if c1 > c2:
-        p1.append(c1)
-        p1.append(c2)
-    else:
-        p2.append(c2)
-        p2.append(c1)
+new_p1, new_p2 = play(p1.copy(), p2.copy())
 
-if len(p1):
-    print(f'{p1=}')
-    print('Part 1:', score(p1))
+if len(new_p1):
+    print(f'{new_p1=}')
+    print('Part 2:', score(new_p1))
 else:
-    print(f'{p2=}')
-    print('Part 1:', score(p2))
+    print(f'{new_p2=}')
+    print('Part 2:', score(new_p2))
