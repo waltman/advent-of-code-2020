5828e827230d1061c3d167ecf6dc23ac31065f5a
Walt Mankowski <waltman@pobox.com>
2020-12-23 19:22:02 -0500

    python code for day 23 part 2

--- day23/crab_cups2.py
+++ day23/crab_cups2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from sys import argv
 
+N = 1_000_000
+MOVES = 10_000_000
+
 class LinkedList:
     def __init__(self, val):
         self.val = val
@@ -8,37 +11,44 @@ class LinkedList:
 
 def create_circle_list(cups):
     d = {}
-    a = [int(c) for c in cups]
-    for i in a:
+    arr = [int(c) for c in cups]
+    for i in arr:
         d[i] = LinkedList(i)
 
-    for i in range(len(a)):
-        d[a[i]].next_node = d[a[(i+1) % len(a)]]
+    for i in range(10,N+1):
+        d[i] = LinkedList(i)
 
-    return d[a[0]]        
+    for i in range(len(arr)-1):
+        d[arr[i]].next_node = d[arr[(i+1) % len(arr)]]
 
-def do_move(clist):
+    # d[arr[8]].next_node = d[arr[0]]
+    d[arr[8]].next_node = d[10]
+    for i in range(10, N):
+        d[i].next_node = d[i+1]
+    d[N].next_node = d[arr[0]]
+    
+    return d[arr[0]], d
+
+def do_move(clist, d):
     # bypass the next 3 nodes
     front = clist.next_node
     back = front.next_node.next_node
     clist.next_node = back.next_node
 
     # which vals were just picked?
-    picked = set([front.val, front.next_node.val, front.next_node.next_node.val])
+    picked = [front.val, front.next_node.val, front.next_node.next_node.val]
 
     # find the destination node num
     dest = clist.val - 1
     if dest == 0:
-        dest = 9
+        dest = N
     while dest in picked:
         dest = dest - 1
         if dest == 0:
-            dest = 9
+            dest = N
 
     # find the destination node
-    p = clist.next_node
-    while p.val != dest:
-        p = p.next_node
+    p = d[dest]
 
     # insert the picked nodes at p
     back.next_node = p.next_node
@@ -51,28 +61,29 @@ def compute_score(clist):
     while clist.val != 1:
         clist = clist.next_node
 
-    # find all the non-1s
-    while True:
-        clist = clist.next_node
-        if clist.val == 1:
-            break
-        else:
-            s += str(clist.val)
-    return s
+    # find the next 2 vals
+    x = clist.next_node.val
+    y = clist.next_node.next_node.val
+    print(f'{x=} {y=}')
+    return x * y
 
-clist = create_circle_list(argv[1])
+clist, d = create_circle_list(argv[1])
+# tmp = clist
+# for _ in range(N):
+#     print(tmp.val)
+#     tmp = tmp.next_node
 
-for move in range(100):
-    do_move(clist)
+for move in range(MOVES):
+    if move % 100_000 == 0:
+        print(f'{move=}')
+    do_move(clist, d)
     clist = clist.next_node
-    print(f'{move=}  ', end='')
-    tmp = clist
-    for _ in range(len(argv[1])):
-        print(f'{tmp.val} ', end='')
-        tmp = tmp.next_node
-    print()
+    # print(f'{move=}  ', end='')
+    # tmp = clist
+    # for _ in range(N):
+    #     print(f'{tmp.val} ', end='')
+    #     tmp = tmp.next_node
+    # print()
 
 # compute score
-print('Part 1:', compute_score(clist))
-
-
+print('Part 2:', compute_score(clist))
